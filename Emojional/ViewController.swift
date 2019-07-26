cd26a4e442a1b57608f6131e83a2a3d7390cdbea
Apple <apple@Apples-MacBook-Pro-94.local>
2019-07-26 11:44:39 -0400

    Not completed Iteration 3

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let emojis = ["🤩": "starry-eyed", "🤠": "haw-yee",]
+    let emojis = ["🤩": "shake it off", "🤠": "vines",]
+    
+    let customMessages = ["shake it off":["My ex-man brought his new girlfriend", "She's like 'oh my God'", "-but I'm just gonna shake it", "And to the fella over there with the hella good hair", "Won't you come on over, baby,", "we can shake, shake, shake, yeah oh"]]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +22,19 @@ class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
             let selectedEmotion = sender.titleLabel?.text
         
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[0]
         
         
-        let alertController = UIAlertController(title: "HII",  message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
         
+        //ALERT CONTROLLER
+        let alertController = UIAlertController(title: "HII",  message: "\(emojiMessage!)", preferredStyle: UIAlertController.Style.alert)
+    
+//        alertController.addAction(UIAlertAction(title:"OK", style:UIAlertAction.Style.default, handler:nil))
+//
+//    alertController.addAction(UIAlertAction(title:"Bye", style:UIAlertAction.Style.default, handler:nil))
+//
+//
+    
             alertController.addAction(UIAlertAction(title:("OK"), style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
