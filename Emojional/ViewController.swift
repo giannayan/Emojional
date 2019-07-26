b5e49034fcd2f10ba9cf5878d122c6c49662234c
Apple <apple@Apples-MacBook-Pro-94.local>
2019-07-26 09:49:33 -0400

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    @IBAction func shakeItOffLyrics(_ sender: Any) {
+    }
+    
+    
+    @IBAction func vineQuotes(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
